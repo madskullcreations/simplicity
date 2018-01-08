9b66a6f2f4d4dc158d88a9bd08921dbba2e129f6
Jon Lennryd <jonlennryd@yahoo.se>
2018-01-08 20:35:34 +0100

    fix.

--- src/Console/InstallerSimplicity.php
+++ src/Console/InstallerSimplicity.php
@@ -40,50 +40,6 @@ class InstallerSimplicity
         'tmp/sessions',
         'tmp/tests'
     ];
-
-    /**
-     * Copy a file, or recursively copy a folder and its contents
-     * @author      Aidan Lister <aidan@php.net>
-     * @version     1.0.1
-     * @link        http://aidanlister.com/2004/04/recursively-copying-directories-in-php/
-     * @param       string   $source    Source path
-     * @param       string   $dest      Destination path
-     * @param       int      $permissions New folder creation permissions
-     * @return      bool     Returns true on success, false on failure
-     */
-    public static function xcopy($source, $dest, $permissions = 0755)
-    {
-        // Check for symlinks
-        if (is_link($source)) {
-            return symlink(readlink($source), $dest);
-        }
-
-        // Simple copy for a file
-        if (is_file($source)) {
-            return copy($source, $dest);
-        }
-
-        // Make destination directory
-        if (!is_dir($dest)) {
-            mkdir($dest, $permissions);
-        }
-
-        // Loop through the folder
-        $dir = dir($source);
-        while (false !== $entry = $dir->read()) {
-            // Skip pointers
-            if ($entry == '.' || $entry == '..') {
-                continue;
-            }
-
-            // Deep copy directories
-            xcopy("$source/$entry", "$dest/$entry", $permissions);
-        }
-
-        // Clean up
-        $dir->close();
-        return true;
-    }
     
     /**
      * Move the zurb foundation files in place.
@@ -122,6 +78,9 @@ class InstallerSimplicity
         $rootDir = dirname(dirname(__DIR__));
 
 // TEST
+$io->write('__DIR__: '.__DIR__);
+$io->write('rootDir: '.$rootDir);
+
         $source = $rootDir."/vendor/zurb/foundation/dist/css/";
         $dest = $rootDir."/webroot/css/";
         static::xcopy($source, $dest);
@@ -158,6 +117,50 @@ class InstallerSimplicity
         }
     }
 
+    /**
+     * Copy a file, or recursively copy a folder and its contents
+     * @author      Aidan Lister <aidan@php.net>
+     * @version     1.0.1
+     * @link        http://aidanlister.com/2004/04/recursively-copying-directories-in-php/
+     * @param       string   $source    Source path
+     * @param       string   $dest      Destination path
+     * @param       int      $permissions New folder creation permissions
+     * @return      bool     Returns true on success, false on failure
+     */
+    public static function xcopy($source, $dest, $permissions = 0755)
+    {
+        // Check for symlinks
+        if (is_link($source)) {
+            return symlink(readlink($source), $dest);
+        }
+
+        // Simple copy for a file
+        if (is_file($source)) {
+            return copy($source, $dest);
+        }
+
+        // Make destination directory
+        if (!is_dir($dest)) {
+            mkdir($dest, $permissions);
+        }
+
+        // Loop through the folder
+        $dir = dir($source);
+        while (false !== $entry = $dir->read()) {
+            // Skip pointers
+            if ($entry == '.' || $entry == '..') {
+                continue;
+            }
+
+            // Deep copy directories
+            static::xcopy("$source/$entry", "$dest/$entry", $permissions);
+        }
+
+        // Clean up
+        $dir->close();
+        return true;
+    }
+    
     /**
      * Create the config/app.php file if it does not exist.
      *
