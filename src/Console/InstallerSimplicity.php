2dfe3bb0eaccd337d8371621673915583c3adc6a
Jon Lennryd <jonlennryd@yahoo.se>
2018-01-08 21:08:54 +0100

    cleaning up.

--- src/Console/InstallerSimplicity.php
+++ src/Console/InstallerSimplicity.php
@@ -1,16 +1,7 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     3.0.0
- * @license   https://opensource.org/licenses/mit-license.php MIT License
+ * Simplicity composer installation script.
+ * 
  */
 namespace App\Console;
 
@@ -27,298 +18,104 @@ use Exception;
  */
 class InstallerSimplicity
 {
-    /**
-     * An array of directories to be made writable
-     */
-    const WRITABLE_DIRS = [
-        'logs',
-        'tmp',
-        'tmp/cache',
-        'tmp/cache/models',
-        'tmp/cache/persistent',
-        'tmp/cache/views',
-        'tmp/sessions',
-        'tmp/tests'
-    ];
-    
-    /**
-     * Move the zurb foundation files in place.
-     *
-     * @param \Composer\Script\Event $event The composer event object.
-     * @throws \Exception Exception raised by validator.
-     * @return void
-     */
-    public static function postInstall(Event $event)
-    {
-// TODO: Allt. Flytta hela kataloger. 
-// Rot: E:\iis\zurbtest\vendor\zurb\foundation
-//  Ignore:
-//    assets
-//    _vendor
-//    customizer
-//    docs
-//    gulp
-//    js
-//    scss
-//    test   (Innehåller deras egna tester av scss, js, visuella tester, etc.)
-//    ...strunta även i alla filer i roten, de är varianter av composer.json, för olika pakethanterare.
-//    
-// Katalogen dist däremot, innehåller css och js, och de borde kunna flyttas till varsin underkatalog i cake's webroot/css resp. /js.
-// NEJ: Katalogen js med, men verkar ha alla plugins. 
-//   <-Den finns som en underkatalog till dist.
-//
-// Så, endast katalogen dist. 
-//    TODO: Jämför innehåller med det du har i simplicity, det är olika versioner, men du känner nog igen strukturen. 
-// TODO: Testa sen publicera och checka ut igen med composer, se att det här scriptet funkar.
+  /**
+   * An array of directories to be made writable
+   */
+  const WRITABLE_DIRS = [
+      'logs',
+      'tmp',
+      'tmp/cache',
+      'tmp/cache/models',
+      'tmp/cache/persistent',
+      'tmp/cache/views',
+      'tmp/sessions',
+      'tmp/tests'
+  ];
+  
+  /**
+   * Move the zurb foundation files into place.
+   *
+   * @param \Composer\Script\Event $event The composer event object.
+   * @throws \Exception Exception raised by validator.
+   * @return void
+   */
+  public static function postInstall(Event $event)
+  {
+    // Root: \vendor\zurb\foundation
+    //  Ignore:
+    //    assets
+    //    _vendor
+    //    customizer
+    //    docs
+    //    gulp
+    //    js
+    //    scss
+    //    test
+    //    ...also ignore files in root. 
+
 // TODO: composer.json-filen bör implementera scripts/post-update-cmd, så att jag kan dra ner senaste versionen av alla grejer och att de då flyttas på plats. (https://getcomposer.org/doc/articles/scripts.md)
-//   <--Skrev det innan, men tänk på att _inte_ versionshantera zurb-filerna. (composer.json ser till att de laddas ner ändå ju.)
 
-        $io = $event->getIO();
+    $io = $event->getIO();
 
-        // We are currently in src/Console folder, but need the root folder of the project.
-        $rootDir = dirname(dirname(__DIR__));
+    // We are currently in src/Console folder, but need the root folder of the project.
+    $rootDir = dirname(dirname(__DIR__));
 
-        // $io->write('__DIR__: '.__DIR__);
-        // $io->write('rootDir: '.$rootDir);
-
-        // Copy the zurb css and js folder to where it belongs.
-        $source = $rootDir."/vendor/zurb/foundation/dist/css/";
-        $dest = $rootDir."/webroot/css/zurb/";
-        static::xcopy($source, $dest);
-
-        $source = $rootDir."/vendor/zurb/foundation/dist/js/";
-        $dest = $rootDir."/webroot/js/zurb/";
-        static::xcopy($source, $dest);
-        
-        static::createAppConfig($rootDir, $io);
-        static::createWritableDirectories($rootDir, $io);
-
-        // ask if the permissions should be changed
-        if ($io->isInteractive()) {
-            $validator = function ($arg) {
-                if (in_array($arg, ['Y', 'y', 'N', 'n'])) {
-                    return $arg;
-                }
-                throw new Exception('This is not a valid answer. Please choose Y or n.');
-            };
-            $setFolderPermissions = $io->askAndValidate(
-                '<info>Set Folder Permissions ? (Default to Y)</info> [<comment>Y,n</comment>]? ',
-                $validator,
-                10,
-                'Y'
-            );
-
-            if (in_array($setFolderPermissions, ['Y', 'y'])) {
-                static::setFolderPermissions($rootDir, $io);
-            }
-        } else {
-            static::setFolderPermissions($rootDir, $io);
-        }
-
-        static::setSecuritySalt($rootDir, $io);
-
-        if (class_exists('\Cake\Codeception\Console\Installer')) {
-            \Cake\Codeception\Console\Installer::customizeCodeceptionBinary($event);
-        }
-    }
-
-    /**
-     * Copy a file, or recursively copy a folder and its contents
-     * @author      Aidan Lister <aidan@php.net>
-     * @version     1.0.1
-     * @link        http://aidanlister.com/2004/04/recursively-copying-directories-in-php/
-     * @param       string   $source    Source path
-     * @param       string   $dest      Destination path
-     * @param       int      $permissions New folder creation permissions
-     * @return      bool     Returns true on success, false on failure
-     */
-    public static function xcopy($source, $dest, $permissions = 0755)
-    {
-        // Check for symlinks
-        if (is_link($source)) {
-            return symlink(readlink($source), $dest);
-        }
-
-        // Simple copy for a file
-        if (is_file($source)) {
-            return copy($source, $dest);
-        }
-
-        // Make destination directory
-        if (!is_dir($dest)) {
-            mkdir($dest, $permissions);
-        }
-
-        // Loop through the folder
-        $dir = dir($source);
-        while (false !== $entry = $dir->read()) {
-            // Skip pointers
-            if ($entry == '.' || $entry == '..') {
-                continue;
-            }
-
-            // Deep copy directories
-            static::xcopy("$source/$entry", "$dest/$entry", $permissions);
-        }
-
-        // Clean up
-        $dir->close();
-        return true;
-    }
+    // $io->write('__DIR__: '.__DIR__);
+    // $io->write('rootDir: '.$rootDir);
     
-    /**
-     * Create the config/app.php file if it does not exist.
-     *
-     * @param string $dir The application's root directory.
-     * @param \Composer\IO\IOInterface $io IO interface to write to console.
-     * @return void
-     */
-    public static function createAppConfig($dir, $io)
-    {
-        $appConfig = $dir . '/config/app.php';
-        $defaultConfig = $dir . '/config/app.default.php';
-        if (!file_exists($appConfig)) {
-            copy($defaultConfig, $appConfig);
-            $io->write('Created `config/app.php` file');
-        }
+    // Copy the zurb css and js folder to where it belongs.
+    $source = $rootDir."/vendor/zurb/foundation/dist/css/";
+    $dest = $rootDir."/webroot/css/zurb/";
+    static::xcopy($source, $dest);
+    $io->write('Copied Zurb css files from "'.$source.'" to "'.$dest.'".');
+
+    $source = $rootDir."/vendor/zurb/foundation/dist/js/";
+    $dest = $rootDir."/webroot/js/zurb/";
+    static::xcopy($source, $dest);
+    $io->write('Copied Zurb js files from "'.$source.'" to "'.$dest.'".');
+  }
+
+  /**
+   * Copy a file, or recursively copy a folder and its contents
+   * @author      Aidan Lister <aidan@php.net>
+   * @version     1.0.1
+   * @link        http://aidanlister.com/2004/04/recursively-copying-directories-in-php/
+   * @param       string   $source    Source path
+   * @param       string   $dest      Destination path
+   * @param       int      $permissions New folder creation permissions
+   * @return      bool     Returns true on success, false on failure
+   */
+  public static function xcopy($source, $dest, $permissions = 0755)
+  {
+    // Check for symlinks
+    if (is_link($source)) {
+      return symlink(readlink($source), $dest);
     }
 
-    /**
-     * Create the `logs` and `tmp` directories.
-     *
-     * @param string $dir The application's root directory.
-     * @param \Composer\IO\IOInterface $io IO interface to write to console.
-     * @return void
-     */
-    public static function createWritableDirectories($dir, $io)
-    {
-        foreach (static::WRITABLE_DIRS as $path) {
-            $path = $dir . '/' . $path;
-            if (!file_exists($path)) {
-                mkdir($path);
-                $io->write('Created `' . $path . '` directory');
-            }
-        }
+    // Simple copy for a file
+    if (is_file($source)) {
+      return copy($source, $dest);
     }
 
-    /**
-     * Set globally writable permissions on the "tmp" and "logs" directory.
-     *
-     * This is not the most secure default, but it gets people up and running quickly.
-     *
-     * @param string $dir The application's root directory.
-     * @param \Composer\IO\IOInterface $io IO interface to write to console.
-     * @return void
-     */
-    public static function setFolderPermissions($dir, $io)
-    {
-        // Change the permissions on a path and output the results.
-        $changePerms = function ($path, $perms, $io) {
-            // Get permission bits from stat(2) result.
-            $currentPerms = fileperms($path) & 0777;
-            if (($currentPerms & $perms) == $perms) {
-                return;
-            }
-
-            $res = chmod($path, $currentPerms | $perms);
-            if ($res) {
-                $io->write('Permissions set on ' . $path);
-            } else {
-                $io->write('Failed to set permissions on ' . $path);
-            }
-        };
-
-        $walker = function ($dir, $perms, $io) use (&$walker, $changePerms) {
-            $files = array_diff(scandir($dir), ['.', '..']);
-            foreach ($files as $file) {
-                $path = $dir . '/' . $file;
-
-                if (!is_dir($path)) {
-                    continue;
-                }
-
-                $changePerms($path, $perms, $io);
-                $walker($path, $perms, $io);
-            }
-        };
-
-        $worldWritable = bindec('0000000111');
-        $walker($dir . '/tmp', $worldWritable, $io);
-        $changePerms($dir . '/tmp', $worldWritable, $io);
-        $changePerms($dir . '/logs', $worldWritable, $io);
+    // Make destination directory
+    if (!is_dir($dest)) {
+      mkdir($dest, $permissions);
     }
 
-    /**
-     * Set the security.salt value in the application's config file.
-     *
-     * @param string $dir The application's root directory.
-     * @param \Composer\IO\IOInterface $io IO interface to write to console.
-     * @return void
-     */
-    public static function setSecuritySalt($dir, $io)
-    {
-        $newKey = hash('sha256', Security::randomBytes(64));
-        static::setSecuritySaltInFile($dir, $io, $newKey, 'app.php');
+    // Loop through the folder
+    $dir = dir($source);
+    while (false !== $entry = $dir->read()) {
+      // Skip pointers
+      if ($entry == '.' || $entry == '..') {
+        continue;
+      }
+
+      // Deep copy directories
+      static::xcopy("$source/$entry", "$dest/$entry", $permissions);
     }
 
-    /**
-     * Set the security.salt value in a given file
-     *
-     * @param string $dir The application's root directory.
-     * @param \Composer\IO\IOInterface $io IO interface to write to console.
-     * @param string $newKey key to set in the file
-     * @param string $file A path to a file relative to the application's root
-     * @return void
-     */
-    public static function setSecuritySaltInFile($dir, $io, $newKey, $file)
-    {
-        $config = $dir . '/config/' . $file;
-        $content = file_get_contents($config);
-
-        $content = str_replace('__SALT__', $newKey, $content, $count);
-
-        if ($count == 0) {
-            $io->write('No Security.salt placeholder to replace.');
-
-            return;
-        }
-
-        $result = file_put_contents($config, $content);
-        if ($result) {
-            $io->write('Updated Security.salt value in config/' . $file);
-
-            return;
-        }
-        $io->write('Unable to update Security.salt value.');
-    }
-
-    /**
-     * Set the APP_NAME value in a given file
-     *
-     * @param string $dir The application's root directory.
-     * @param \Composer\IO\IOInterface $io IO interface to write to console.
-     * @param string $appName app name to set in the file
-     * @param string $file A path to a file relative to the application's root
-     * @return void
-     */
-    public static function setAppNameInFile($dir, $io, $appName, $file)
-    {
-        $config = $dir . '/config/' . $file;
-        $content = file_get_contents($config);
-        $content = str_replace('__APP_NAME__', $appName, $content, $count);
-
-        if ($count == 0) {
-            $io->write('No __APP_NAME__ placeholder to replace.');
-
-            return;
-        }
-
-        $result = file_put_contents($config, $content);
-        if ($result) {
-            $io->write('Updated __APP_NAME__ value in config/' . $file);
-
-            return;
-        }
-        $io->write('Unable to update __APP_NAME__ value.');
-    }
+    // Clean up
+    $dir->close();
+    return true;
+  }
 }
